b5ef36c4017dadd013530190bf955011b85b2485
dieperid <david.dieperink@ik.me>
2026-01-27 18:21:25 +0100

    feat: added footer

---
+++ src/lib/data.ts
@@ -0,0 +1,10 @@
+export const socialLinks = {
+  instagram: "https://www.instagram.com/atelier_endurance/",
+  david_website: "https://www.daviddieperink.ch",
+};
+
+export const menuItems = [
+  { label: "Accueil", href: "/" },
+  { label: "Coaching", href: "/coaching" },
+  { label: "Témoignages", href: "/testimonials" },
+];
