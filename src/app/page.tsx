28035889344c2991bcf9cc695f7070048cfb7c61
dieperid <david.dieperink@ik.me>
2026-01-28 20:09:28 +0100

    refactor: removed HomeCoach section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import HeroLanding from "@/components/HeroLanding";
 import HomeAboutSection from "@/components/home/HomeAboutSection";
-import HomeCoachesSection from "@/components/home/HomeCoachesSection";
 
 export default function Home() {
   return (
     <div>
       <HeroLanding />
       <HomeAboutSection />
-      <HomeCoachesSection />
     </div>
   );
 }
