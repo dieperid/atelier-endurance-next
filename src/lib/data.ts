cde1e4744befe05af1e126be58c6be28dd241362
dieperid <david.dieperink@ik.me>
2026-01-28 19:17:40 +0100

    feat: added coaches data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -15,3 +15,59 @@ export const coachingPlans = [
   { title: "Plan mensuel", href: "/coaching/mensuel" },
 ];
 
+export const coaches = [
+  {
+    name: "David Dieperink",
+    imageSrc: "/assets/img/hero-bg.jpeg",
+    imageAlt: "David",
+    bio: [
+      "Ancien fumeur et fêtard, j’ai pris un tournant décisif en 2021 en me reprenant en main, avec une perte de poids comme premier objectif. Poussé par mon frère, j’ai tenté mon premier 10 km… et j'ai détesté la course à pied ! C’est alors que je me suis tourné vers les sports de combat, que j’ai pratiqués jusqu’en 2023.",
+      "Mais mon endurance restait un frein, et une nouvelle fois, mon frère m’a embarqué dans sa passion : le vélo. De fil en aiguille, j’ai commencé à rouler, puis à courir, et sans même m’en rendre compte, je me suis lancé des défis toujours plus grands et ambitieux : courir mon premier marathon, terminer un trail de 70 km…",
+      "Aujourd’hui, je suis tombé amoureux des sports d’endurance. La physiologie et l’entraînement me passionnent, et cette soif de progression m’a donné envie d’aider les autres à atteindre leurs propres objectifs.",
+    ],
+    badges: [
+      {
+        label: "Trail Verbier St Bernard by UTMB 2025 - X-Alpine (26h29)",
+        href: "https://utmb.world/utmb-index/races/669.trailverbierstbernardbyutmbx-alpine.2025",
+      },
+      {
+        label: "Trail du Mont Bally 2025 (27km) - 5ème scratch",
+        href: "https://www.softtiming.ch/resultats/callHtml.php?page=Class_gen27.xml&retour=/resultats/index.php&type=TC&reper=gimel25/&ext=xml",
+      },
+      {
+        label: "SwissPeaks 70k 2024 - 12ème scratch",
+        href: "https://www.ultratiming.live/en/evenement/spt-2024/epreuve/70/resultats",
+      },
+      { label: "Marathon (2024) - 3h07" },
+    ],
+  },
+  {
+    name: "Anthony Dieperink",
+    imageSrc: "/assets/img/hero-bg.jpeg",
+    imageAlt: "Anthony",
+    reverse: true,
+    bio: [
+      "Durant la période Covid, j’ai ressenti le besoin de me remettre en forme. Confiné et lassé de l’inactivité, j’ai décidé de me lancer un défi : la course à pied. Mes premiers kilomètres ont été laborieux, mais petit à petit, j’ai pris goût à l’effort et aux sensations qu’il procurait.",
+      "Après plusieurs mois de pratique, l’envie de diversifier mon entraînement s’est imposée. En 2022, je suis monté sur un vélo, sans me douter que ce serait une révélation. D’abord par curiosité, puis par passion, j’ai enchaîné les sorties, allongeant progressivement les distances. Chaque coup de pédale était une nouvelle étape, un nouveau défi à relever.",
+      "Aujourd’hui, le sport est devenu bien plus qu’un simple passe-temps : c’est un mode de vie. L’endurance, la progression, la découverte de mes propres limites me motivent au quotidien, et cette passion ne cesse de grandir. Fort de cette expérience, j’ai désormais envie de partager ce chemin avec d’autres, de les accompagner dans leur propre transformation et de les aider à atteindre leurs objectifs, quels qu’ils soient.",
+    ],
+    badges: [
+      {
+        label: "TDS Superfondo 2025 - 1er scratch 🏆",
+        href: "https://www.mso.swiss/events/2390-tour-des-stations/results?type=CATEGORIES&typeId=38383",
+      },
+      {
+        label: "UCI Granfondo Suisse 2025 - 8ème scratch",
+        href: "https://www.mso.swiss/events/2366-uci-granfondo-suisse/results?type=SCRATCH&typeId=14176",
+      },
+      {
+        label: "TDS Superfondo 2024 - 5ème scratch",
+        href: "https://www.mso.swiss/events/2187-tour-des-stations/results?type=SCRATCH&typeId=13256",
+      },
+      {
+        label: "TDS Superfondo 2023 - 3ème scratch 🥉",
+        href: "https://www.mso.swiss/events/1777-tour-des-stations/results?type=SCRATCH&typeId=11765",
+      },
+    ],
+  },
+];
