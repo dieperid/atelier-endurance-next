cb3b15e26b92acea9019d5d2634467e73d89f2a8
dieperid <david.dieperink@ik.me>
2026-01-29 11:20:40 +0100

    style: removed useless class and added pt and pb

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -6,16 +6,14 @@ export const metadata = {
 
 export default function TeamPage() {
   return (
-    <main className="pt-6 pb-12">
-      <div className="container mx-auto px-6 sm:px-16">
-        <div className="mx-auto max-w-6xl">
-          <header className="space-y-4 md:space-y-6">
-            <h1 className="italic text-4xl font-semibold tracking-tight md:text-6xl lg:text-7xl xl:text-8xl">
-              L&apos;équipe
-            </h1>
-            <div className="h-1 w-48 rounded-full bg-linear-to-r from-black via-slate-500 to-transparent" />
-          </header>
-        </div>
+    <main className="container mx-auto px-6 sm:px-16 pt-6 pb-12">
+      <div className="mx-auto max-w-6xl">
+        <header className="space-y-4 md:space-y-6">
+          <h1 className="italic text-4xl font-semibold tracking-tight md:text-6xl lg:text-7xl xl:text-8xl">
+            L&apos;équipe
+          </h1>
+          <div className="h-1 w-48 rounded-full bg-linear-to-r from-black via-slate-500 to-transparent" />
+        </header>
       </div>
 
       <TeamSection />
