716486f10358c49da1279d1b270f11aea4eaaa59
dieperid <david.dieperink@ik.me>
2026-01-29 11:21:58 +0100

    style: updated responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,25 +41,28 @@ export default function Header() {
       onMenuOpenChange={setIsMenuOpen}
       height={"6rem"}
       isBlurred={!isHome}
+      classNames={{
+        wrapper: "container max-w-6xl mx-auto px-6 sm:px-16 xl:px-0",
+      }}
       className={`font-medium w-full z-50 ${
         isHome
           ? isMenuOpen
-            ? "bg-white dark:bg-black absolute top-0 left-0"
+            ? "bg-white absolute top-0 left-0"
             : "bg-transparent absolute top-0 left-0"
-          : "bg-white dark:bg-black sticky top-0"
+          : "bg-white sticky top-0"
       }`}
     >
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
+          className="lg:hidden"
         />
         <NavbarBrand>
           <Logo />
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden lg:flex gap-4" justify="center">
         {menuItems.map((item) => {
           const isActive = pathname === item.href;
           return (
@@ -87,32 +90,29 @@ export default function Header() {
             as={Link}
             color="primary"
             href="https://forms.gle/gVNSToD87qrQ41wr6"
-            className="text-lg"
+            className="sm:text-lg"
             target="_blank"
           >
             Nous contacter
           </Button>
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2
-                  ? "primary"
-                  : index === menuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-              }
-              href={item.href}
-              size="lg"
-            >
-              {item.label}
-            </Link>
-          </NavbarMenuItem>
-        ))}
+      <NavbarMenu className="sm:px-16">
+        {menuItems.map((item, index) => {
+          const isActive = pathname === item.href;
+          return (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                href={item.href}
+                className={`text-xl hover:underline ${
+                  isActive ? "primary font-normal" : "text-foreground"
+                }`}
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          );
+        })}
       </NavbarMenu>
     </Navbar>
   );
