cd1b36b3eb93201cf018fc1e867337c2072cfde9
dieperid <david.dieperink@ik.me>
2026-01-31 11:29:26 +0100

    feat: updated image link

--- src/lib/testimonialsData.ts
+++ src/lib/testimonialsData.ts
@@ -3,30 +3,30 @@ export const testimonials = [
     name: "Sacha Christen",
     description:
       "Ce qui fait la force de David, c'est sans aucun doute son suivi personnalisé qui me permet pas-à-pas de me rapprocher de mes objectifs. Au-delà de ses connaissances et de ses conseils techniques, c'est son soutien et sa disponibilité qui font toute la différence.<br> Ses séances sont à la fois exigeantes et plaisantes, de plus, il reste toujours à mon écoute pour que le plaisir soit toujours présent !",
-    image: "/assets/img/hero-bg.jpeg",
+    image: "/assets/img/testimonials/SachaChristen.jpeg",
   },
   {
     name: "Jérémie Boillat",
     description:
       "Cela fait environ cinq ans que je me suis mis au trail, mais sans jamais réellement suivre un plan d’entraînement structuré. Je progressais, certes, mais lentement, et j’avais tendance à me décourager facilement, faute de discipline et de cadre. Puis, j’ai découvert l’Atelier de l’Endurance et j’ai vu en cette initiative une belle opportunité pour m’engager plus sérieusement dans ma pratique, d’autant plus que je connaissais déjà David, ce qui m’a tout de suite mis en confiance.<br><br> Depuis que j’ai intégré l’Atelier, j’ai ressenti un véritable changement : je suis plus discipliné, plus régulier et surtout plus motivé que jamais. Même s’il est encore trop tôt pour observer des résultats flagrants sur mes performances, je sens déjà une nette amélioration dans mon état d’esprit et ma rigueur. Je suis convaincu que cette dynamique positive me permettra d’atteindre de nouveaux objectifs avec confiance et détermination.",
-    image: "/assets/img/hero-bg.jpeg",
+    image: "/assets/img/testimonials/JeremieBoillat.jpeg",
   },
   {
     name: "Killian Good",
     description:
       "Au début, je n’aimais pas vraiment la course à pied… J’y allais à reculons, sans vraie motivation, et surtout sans structure. Mais depuis que je suis suivi par l’atelier de l’endurance, tout a changé. Les entraînements sont adaptés, progressifs et surtout motivants.<br> Aujourd’hui, courir est devenu un vrai plaisir et même un défi que j’ai hâte de relever chaque jour. Je recommande à 100 % !",
-    image: "/assets/img/hero-bg.jpeg",
+    image: "/assets/img/testimonials/KillianGood.jpeg",
   },
   {
     name: "Grégoire Palthey",
     description:
       "David m'a accompagné dans la préparation d’un trail avec un programme de course à pied sur mesure, complété par du renforcement musculaire.<br> Son vrai point fort, c’est son suivi : toujours disponible, à l’écoute, et capable d’adapter en temps réel selon mes progrès, mes objectifs et ma forme du moment.",
-    image: "/assets/img/hero-bg.jpeg",
+    image: "/assets/img/testimonials/GregoirePalthey.jpeg",
   },
   {
     name: "Louis Emeri",
     description:
       "Étant un passionné de sport d’endurance, ça fait maintenant 6 ans que j’en pratique. J’ai toujours eu l’habitude d’organiser mes séances de sport en improvisant en fonction de mes envies et du temps à disposition. En vue d’une participation à un IRONMAN 70.3, j’ai pris la décision de faire appel à un coach afin d’améliorer mes performances.<br><br> Je suis tombé sur l’Atelier de l’Endurance et j’ai tout de suite pris contact avec David. Ses conseils m’ont particulièrement aidé sur plusieurs aspects. Mes compétences physiques se sont nettement améliorées et le plan proposé me permet de moins me prendre la tête et d’uniquement me concentrer sur les séances. De plus, le coach offre un vrai suivi par message et répond à toutes les questions et préoccupations que j’ai pu avoir.",
-    image: "/assets/img/hero-bg.jpeg",
+    image: "/assets/img/testimonials/LouisEmeri.jpg",
   },
 ];
